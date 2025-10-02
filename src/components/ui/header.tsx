19f3edcca2d38f91de2695a6e55d6af5346cc08a
Santhoshkumar <119757651+KumarSanthosh16@users.noreply.github.com>
2025-10-02 21:30:42 +0530

    added toggle switch

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,8 +1,12 @@
-import { ArrowLeft } from '@phosphor-icons/react';
+import { ArrowLeft, Moon, SunDim } from '@phosphor-icons/react';
 import { useEffect, useState } from 'react';
+import { Tabs, TabsList, TabsTrigger } from '../../components/ui/tabs';
+import type { Theme } from '../theme-context';
+import { useTheme } from '../use-theme';
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -15,7 +19,7 @@ const Header = () => {
 
   return (
     <div
-      className={`p-6 flex justify-between items-start flex-col gap-4 fixed lg:sticky top-0 mx-auto z-50 transition-all duration-300 bg-white dark:bg-[var(--q3-surface-default-v3)] w-full ${
+      className={`p-6 flex justify-between items-start flex-col gap-4 fixed lg:sticky top-0 mx-auto z-50 transition-all duration-300 w-full ${
         isScrolled
           ? 'border border-[var(--q3-surface-default-v2)] dark:border-[var(--q3-stroke-light)] backdrop-blur-2xl rounded-br-2xl rounded-bl-2xl'
           : 'border border-transparent'
@@ -39,6 +43,26 @@ const Header = () => {
           (Old) / Analysis / Leaderboard
         </p>
       )}
+      <Tabs
+        value={theme}
+        onValueChange={(value) => setTheme(value as Theme)}
+        className='w-fit absolute top-3 right-3 bg-transparent'
+      >
+        <TabsList>
+          <TabsTrigger
+            value='light'
+            className='!text-white data-[state=active]:!text-black !rounded-full data-[state=active]:!bg-[var(--sidebar-ring)]'
+          >
+            <SunDim size={32} />
+          </TabsTrigger>
+          <TabsTrigger
+            value='dark'
+            className='!text-black data-[state=active]:!text-white !rounded-full data-[state=active]:!bg-[var(--sidebar-ring)]'
+          >
+            <Moon size={32} />
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
     </div>
   );
 };
